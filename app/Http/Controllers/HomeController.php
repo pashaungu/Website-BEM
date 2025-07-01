cfe74b74a485713d1bf3ff1527aff6a502d378e1
pasha aditya <pashaaditya157@gmail.com>
2025-07-01 13:56:32 +0700

    nunfu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,10 +10,10 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // $programs = Program::latest()->take(3)->get(); // hanya ambil 3 program unggulan
-        // $kalenders = Kalender::latest()->take(3)->get(); // hanya ambil 3 kegiatan terbaru
+        $programs = Program::latest()->take(3)->get(); // hanya ambil 3 program unggulan
+        $kalenders = Kalender::latest()->take(3)->get(); // hanya ambil 3 kegiatan terbaru
 
-        // return view('pages.home', compact('programs', 'kalenders'));
-        return view('pages.home');
+        return view('pages.home', compact('programs', 'kalenders'));
+        
     }
 }
